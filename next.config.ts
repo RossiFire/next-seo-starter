f14d75362e0d76f6ab65e12826b9338616ebd051
Daniele Rossino <daniele.rossino10@gmail.com>
2024-10-27 10:29:17 +0100

    feat: Supporting NextJs 15

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from 'next';
+ 
+const nextConfig: NextConfig = {
+  images: {
+    // This is usually always present in the config. Uncomment if needed
+    // remotePatterns: [ 
+    //   {
+    //     protocol: "https",
+    //     hostname: "**",
+    //   },
+    // ],
+  },
+};
+ 
+export default nextConfig;
