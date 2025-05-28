40a883deade0a35fe10b00c0934e1edb11021b0b
Daniele Rossino <fratsavio.dr@gmail.com>
2025-05-28 20:24:44 +0200

    fix: robots file

---
+++ app/robots.ts
@@ -0,0 +1,36 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+    return {
+
+        // Base rules
+        rules: [
+            {
+                userAgent: "*",
+                allow: "/",
+                disallow: [
+                    "/api/*",
+                    "/static/*",
+                    "/*.json$",
+                    "/*.xml$",
+                    "/admin/*",
+                    "/private/*"
+                ]
+            },
+
+            // Those rules are for blocking GPT and Common Crawl's web crawlers from indexing the site
+            // If you want to allow them, you can remove the rules below
+            {
+                userAgent: "GPTBot",
+                disallow: "/"
+            },
+
+            {
+                userAgent: "CCBot",
+                disallow: "/"
+            }
+        ],
+        sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
+        host: process.env.NEXT_PUBLIC_BASE_URL
+    }
+}
