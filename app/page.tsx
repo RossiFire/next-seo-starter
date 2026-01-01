cbfbfd06fc10c0715de523e1819705868ba8c664
Daniele Rossino <fratsavio.dr@gmail.com>
2026-01-01 21:43:17 +0100

    feat: updated homepage

--- app/page.tsx
+++ app/page.tsx
@@ -27,52 +27,65 @@ export default function Home() {
 					</Link>
 				</div>
 			</div>
-			<div className="col-span-7">
-				<ul
-					className={cn(
-						"text-sm md:text-base tracking-widest my-12 mx-auto px-4 md:px-0 font-dm-sans"
-					)}
-				>
-					<li>
-						<span className="text-green-700">&#10003;</span> Husky for shared Git hooks across the
-						team.
-					</li>
-					<li>
-						<span className="text-green-700">&#10003;</span> Lint-staged for running linting and
-						formatting before committing.
-					</li>
-					<li>
-						<span className="text-green-700">&#10003;</span> Conventinal commits to enforce commit
-						messages convention.
-					</li>
-					<li>
-						<span className="text-green-700">&#10003;</span> Biome for linting and formatting.
-					</li>
-					<li>
-						<span className="text-green-700">&#10003;</span> SEO-Oriented with metadata and
-						microdata.
-					</li>
-					<li>
-						<span className="text-green-700">&#10003;</span> Sitemap, robots.txt and Manifest.
-					</li>
-					<li>
-						<span className="text-green-700">&#10003;</span> Google Analytics natively integrated
-						with Next.js.
-					</li>
-					<li>
-						<span className="text-green-700">&#10003;</span> Tailwindcss for styling.
-					</li>
-					<li>
-						<span className="text-green-700">&#10003;</span> Typescript; of course.
-					</li>
-					<li>
-						<span className="text-green-700">&#10003;</span> Framer Motion for easy and stunning
-						animations.
-					</li>
-					<li>
-						<span className="text-green-700">&#10003;</span> Jest suite to TDD your code.
-					</li>
-				</ul>
+			<div className="col-span-7 grid place-items-center">
+				<div className="flex flex-col items-start justify-center gap-6">
+					<h2 className="text-transparent bg-clip-text bg-linear-to-r from-primary via-gray-950 to-secondary font-bbh-bartle text-xl">Features</h2>
+					<div className="flex flex-col gap-2">
+						<span className="text-sm font-dm-sans font-medium text-gray-600">Core</span>
+						<ul className={cn("text-sm md:text-base tracking-widest font-dm-sans")}>
+							<li>
+								<span className="text-green-700">&#10003;</span> <strong>Next.js</strong> always up to date.
+							</li>
+							<li>
+								<span className="text-green-700">&#10003;</span> <strong>Tailwind CSS</strong> for styling.
+							</li>
+							<li>
+								<span className="text-green-700">&#10003;</span> <strong>Docker</strong> setup, for easy deployment.
+							</li>
+							<li>
+								<span className="text-green-700">&#10003;</span> <strong>Framer Motion</strong> for easy and stunning
+								animations.
+							</li>
+							<li>
+								<span className="text-green-700">&#10003;</span> <strong>Jest</strong> suite to TDD your code.
+							</li>
+						</ul>
+					</div>
+					<div className="flex flex-col gap-2">
+						<span className="text-sm font-dm-sans font-medium text-gray-600">Workflow</span>
+						<ul className={cn("text-sm md:text-base tracking-widest font-dm-sans")}>
+							<li>
+								<span className="text-green-700">&#10003;</span> <strong>Husky</strong> for shared Git hooks across the
+								team.
+							</li>
+							<li>
+								<span className="text-green-700">&#10003;</span> <strong>Lint-staged</strong> for linting and
+								formatting committed files.
+							</li>
+							<li>
+								<span className="text-green-700">&#10003;</span> <strong>Conventional commits</strong> to enforce commit
+								messages convention.
+							</li>
+							<li>
+								<span className="text-green-700">&#10003;</span> <strong>Biome</strong> for linting and formatting. <i className="text-xs">(~35x faster than Prettier)</i>
+							</li>
+						</ul>
+					</div>
+					<div className="flex flex-col gap-2">
+						<span className="text-sm font-dm-sans font-medium text-gray-600">SEO & Analytics</span>
+						<ul className={cn("text-sm md:text-base tracking-widest font-dm-sans")}>
+							<li>
+								<span className="text-green-700">&#10003;</span> <strong>Metadata</strong> and <strong>Microdata</strong> utilities for SEO.
+							</li>
+							<li>
+								<span className="text-green-700">&#10003;</span> <strong>Sitemap.xml</strong>, <strong>robots.txt</strong> and <strong>Manifest.xml</strong>.
+							</li>
+							<li>
+								<span className="text-green-700">&#10003;</span> <strong>Google Analytics</strong> natively integrated with Next.js.
+							</li>
+						</ul>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
